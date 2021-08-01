dce037e9dd478063684121cf3f2e1f4feccb8901
Parth <parthks@umich.edu>
2021-08-01 22:00:29 +0530

    cloud functions working now

--- functions/helpers/mailTransport.js
+++ functions/helpers/mailTransport.js
@@ -11,9 +11,9 @@ const gmailEmail = "admin@overbooked.in"
 exports.getMailTransport = async function initializeMailTransport() {
 
     const OAuth2 = google.auth.OAuth2;
-    const clientID = "57306778842-4g0vptpp5g7eg9jfajkapkegfdd4hhtu.apps.googleusercontent.com";
-    const clientSecret = "o4hLJ2M5nwSidzA_6_1ekXD6";
-    const refreshToken = "1//04UQ2yxHNw1NICgYIARAAGAQSNwF-L9Iris0b1NNKYk-Xj1I-VCyENXz11IACpJVpRd7lORhbnPakAUPZNKemXe8vTJlLhG381WU"
+    const clientID = "376098439791-dfjdgit8okorcfb4dnrn8g2snr6ckmej.apps.googleusercontent.com";
+    const clientSecret = "kgJw79w-fDT-W_tWXz5tEsaD";
+    const refreshToken = "1//04unIobVCPD5RCgYIARAAGAQSNwF-L9IrBU7TTuVoPkjmXN6ReNakljNZOIcM51zdP0eemK-7D3W8O40txwNNxoS4w8Xyyvw38Hc"
   
     const oauth2Client = new OAuth2(
       clientID, //client Id
@@ -25,8 +25,11 @@ exports.getMailTransport = async function initializeMailTransport() {
       refresh_token: refreshToken
     });
   
-    const tokens = await oauth2Client.refreshAccessToken();
-    const accessToken = tokens.credentials.access_token;
+    // const tokens = await oauth2Client.refreshAccessToken();
+    // const accessToken = tokens.credentials.access_token;
+    const accessToken = oauth2Client.getAccessToken()
+
+
   
     // const cors = require('cors')({origin: true});
   
