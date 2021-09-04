7e292f615fd7dd5fd4f9c0280f1c46400823eba9
Parth <parthks@umich.edu>
2021-09-04 19:39:39 +0530

    about us

--- pages/about-us.js
+++ pages/about-us.js
@@ -11,7 +11,7 @@ export default function AboutUs() {
         <Typography variant="h3">About Us</Typography><br />
 
         <Typography>
-        I am Krish Lambah, the founder of Overbooked! I am in grade 9 in the Cathedral and John Connon School. I love reading and have always had more books than I can fit in my shelf. I know that there are many others like me. I have created Overbooked to help all of us give away the books we no longer have use for. Through Overbooked, I want to create a community of people who would like to recycle their books and help preserve the environment at the same time.         
+        I am Krish Lambah, co-founder of Overbooked! I am in grade 9 in the Cathedral and John Connon School. I love reading and have always had more books than I can fit in my shelf. I know that there are many others like me. I have created Overbooked to help all of us give away the books we no longer have use for. Through Overbooked, I want to create a community of people who would like to recycle their books and help preserve the environment at the same time.         
         <br />
         Enjoy reading! 
         <br />
@@ -21,7 +21,7 @@ export default function AboutUs() {
         <br />
 
         <Typography>
-        I am Rhea Lambah, co-founder of Overbooked. I am a senior in the Cathedral and John Connon School. I joined my brother Krish in creating this platform. I have always wanted to see my used books being of relevance to others, as against sitting on my shelf. Through Overbooked, we will target creating a book reading community, that benefits from each other and also helps in keeping our planet more green! 
+        I am Rhea Lambah, co-founder of Overbooked! I am a senior in the Cathedral and John Connon School. I joined my brother Krish in creating this platform. I have always wanted to see my used books being of relevance to others, as against sitting on my shelf. Through Overbooked, we will target creating a book reading community, that benefits from each other and also helps in keeping our planet more green! 
         <br />
         Let’s have fun and read more!
         <br />
